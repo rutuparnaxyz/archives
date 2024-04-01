adcde454d3dfdb1ecf1a51eb871c8d508f61162b
Rutuparna Rout <rutuparna.rout@nagarro.com>
2024-04-01 19:22:00 +0530

    Removed Secrets

--- studit/studit-admin/src/environments/environment.ts
+++ studit/studit-admin/src/environments/environment.ts
@@ -5,8 +5,6 @@
 export const environment = {
   production: false,
    apiUrl:"http://ec2-65-0-185-102.ap-south-1.compute.amazonaws.com:3001/",
-   AWS_ACCESS_KEY_ID:'AKIAIPGULHBIQP3AA7YA',
-   AWS_SECREAT_ACCESS_KEY:'LfARbsaXV2bWdTxdO+sb19k9vy7XbTkrIvC5ceL5',
    AWS_REGION:'ap-south-',
    BUCKET_NAME:'300betassets'
    
